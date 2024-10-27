00c7231f2a3a19d44090c16e03dddd9e928b2e60
AlexGoIT23 <lawyer.csat@gmail.com>
2024-10-27 16:19:13 +0200

    done

--- js/task-02.js
+++ js/task-02.js
@@ -3,3 +3,18 @@
 // Функція створює об"єкт товару, додає йому унікальний ідентифікатор у властивість id та викликає callback,
 // передаючи йому створений об"єкт
 // showProduct(product)  - колбек, що приймає об"єкт продукта та виводить його у консоль
+
+const makeProduct = (name, price, callback) => {
+  const product = {
+    name,
+    price,
+    id: Math.random(),
+  };
+  callback(product);
+};
+
+const showProduct = (product) => {
+  console.log(product);
+};
+
+makeProduct("apple", 100, showProduct);
